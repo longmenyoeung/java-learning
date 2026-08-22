8f5f0009cec7a5dfeefd386c9f871ce5498f9d48
longmenyoeung <longmenyoeung@gmail.com>
2026-08-22 20:38:38 +0700

    modern switch and tranditional switch case with string

--- controllstatement/switchCase.java
+++ controllstatement/switchCase.java
@@ -6,22 +6,47 @@ public class switchCase {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        int day ;
-        System.out.print("Enter day :");
-        day = input.nextInt();
+//        int day ;
+//        System.out.print("Enter day :");
+//        day = input.nextInt();
+//
+//        switch (day) {
+//            case 1 :
+//                System.out.println("Monday");
+//                break;
+//            case 2 :
+//                System.out.println("Tousday");
+//                break;
+//            case 3 :
+//                System.out.println("Wednesday");
+//                break;
+//            default:
+//                System.out.println("Invalid input.");
+//                break;
+//        }
 
-        switch (day) {
-            case 1 :
-                System.out.println("Monday");
+        //Modern Switch Expression with String
+        String role;
+        System.out.print("Enter role :"); role =input.nextLine();
+        switch (role){
+            case "admin" -> System.out.println("Welcome admin.");
+            case "manager" -> System.out.println("Welcome manager");
+            default -> System.out.println("Unknow role.");
+        }
+
+        //Tranditional Switch with string
+        String name ;
+        System.out.print("Enter user name :"); name = input.nextLine();
+
+        switch (name) {
+            case "men" :
+                System.out.println("u are our staff");
                 break;
-            case 2 :
-                System.out.println("Tousday");
-                break;
-            case 3 :
-                System.out.println("Wednesday");
+            case "kaka" :
+                System.out.println("u are our staff");
                 break;
             default:
-                System.out.println("Invalid input.");
+                System.out.println("Not my staff");
                 break;
         }
     }
