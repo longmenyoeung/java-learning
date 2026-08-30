55468d7462a9d29a667859063d65a49e4489e4d3
longmenyoeung <longmenyoeung@gmail.com>
2026-08-30 19:04:26 +0700

    class and obj with brand exericse

---
+++ oop/Brand.java
@@ -0,0 +1,34 @@
+package LessonJava.oop;
+
+import java.util.Scanner;
+
+public class Brand {
+    int id;
+    String name;
+    int code ;
+    String desc;
+    String fromCountry;
+
+
+    public void input() {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("====== Input information ======");
+        System.out.print("Enter ID = "); id = input.nextInt();
+        input.nextLine();
+        System.out.print("Enter Name = "); name = input.nextLine();
+        System.out.print("Enter Code = "); code = input.nextInt();
+        System.out.print("Enter Desc = "); desc = input.nextLine();
+        input.nextLine();
+        System.out.print("Enter From Country = "); fromCountry = input.nextLine();
+    }
+
+
+    public void  header() {
+        System.out.printf("%10s %15s %10s %17s %15s \n", "ID", "NAME", "CODE", "DESCRIPTION", "FROM COUNTRY");
+    }
+
+    public void output() {
+        System.out.printf("%10d %15s %10d %17s %15s \n", id,name,code,desc,fromCountry);
+    }
+}
--- oop/main.java
+++ oop/main.java
@@ -4,14 +4,19 @@ package LessonJava.oop;
 public class main {
     public static void main(String[] args) {
         //Create obj from class
-        Test test1 = new Test();
-        //Call method
-        test1.input(); //--> access class member
-        test1.output(); // -> access class member
+//        Test test1 = new Test();
+//        //Call method
+//        test1.input(); //--> access class member
+//        test1.output(); // -> access class member
 
-        Demo01 demo1 = new Demo01();
-        demo1.input();
-        demo1.header();
-        demo1.output();
+//        Demo01 demo1 = new Demo01();
+//        demo1.input();
+//        demo1.header();
+//        demo1.output();
+
+        Brand brand1 = new Brand();
+        brand1.input();
+        brand1.header();
+        brand1.output();
     }
 }
