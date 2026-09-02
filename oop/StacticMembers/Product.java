92e2c5c15bca6bd5691f7fafa81ea19be3a8fd93
longmenyoeung <longmenyoeung@gmail.com>
2026-09-02 15:40:51 +0700

    keyword final

--- oop/StacticMembers/Product.java
+++ oop/StacticMembers/Product.java
@@ -2,4 +2,9 @@ package LessonJava.oop.StacticMembers;
 
 public class Product {
     static public String name = "KPS";
+    final private double discount = 10;
+
+//    public void display(){
+//        discount = 90; // cant change
+//    }
 }
